6404a3996b4e005f6f940d9601d7c1d6182fba90
Pradeep Kumar <prad6g@gmail.com>
2023-09-30 19:26:07 +0530

    add projects, contact, about page to website

---
+++ my-app/app.js
@@ -0,0 +1,11 @@
+function sayHello(params) {
+    alert("hi");
+}
+
+//prompt("Enter your user name")
+let username = "1pradeep" 
+console.log(typeof username)
+
+age = parseInt(username);
+typeof age
+console.log(age)
